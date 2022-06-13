3f68a6d4d71bad8db3869af58fb1b87ec1131d4f
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-13 20:25:21 +0200

    updates context

--- api/treuf.py
+++ api/treuf.py
@@ -1,9 +1,10 @@
 from app import create_app, db
-from app.models import User, Borrowing, Item
+from app.models import User, Borrowing, Item, Role
 
+# creates the app instance to be used when launching with flask run
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Borrowing": Borrowing, "Item": Item}
+    return {"db": db, "User": User, "Borrowing": Borrowing, "Item": Item, "Role": Role}
