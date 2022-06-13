b9cfb86767b1d78945faeb069c4035e65188f43b
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-13 20:26:04 +0200

    format and imports

--- api/app/api/__init__.py
+++ api/app/api/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('api', __name__)
+bp = Blueprint("api", __name__)
 
-from app.api import users, errors, tokens
-
+from app.api import users, borrowings, items, errors, tokens
