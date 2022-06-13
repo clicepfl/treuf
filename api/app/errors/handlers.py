1d4ee8d76daf0dc2ca89890987510164bd3c2ac1
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-13 20:26:19 +0200

    api corrected handlers

--- api/app/errors/handlers.py
+++ api/app/errors/handlers.py
@@ -1,13 +1,20 @@
 from app import db
 from app.errors import bp
 from app import email
+from app.api.errors import error_response as api_error_response
+
+
+@bp.app_errorhandler(403)
+def forbidden_error(error):
+    return api_error_response(403)
+
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
-    return "", 404
+    return api_error_response(404)
 
 
 @bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return "", 500
+    return api_error_response(500)
