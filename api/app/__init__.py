af63a033b16ec537ae916616be644e9d7920076c
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-13 20:25:31 +0200

    documents

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from logging.handlers import RotatingFileHandler, SMTPHandler
-from time import time
 
 from config import Config
 from flask import Flask
@@ -15,6 +14,7 @@ mail = Mail()
 
 
 def create_app(config_class=Config):
+    """Factory pattern for app instance construction. Constructs the app from a Config objects and registers the blueprint modules."""
     app = Flask(__name__)
     app.config.from_object(config_class)
 
@@ -22,15 +22,19 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     mail.init_app(app)
 
-    from app.errors import bp as error_bp
-
-    app.register_blueprint(error_bp)
-
     from app.api import bp as api_bp
 
     app.register_blueprint(api_bp, url_prefix="/api")
 
+    from app.errors import bp as error_bp
+
+    app.register_blueprint(error_bp)
+
+    """Handles application logs by mail and in files when deployed."""
     if not app.debug and not app.testing:
+        # we cannot yet have mail logs with a SMTP over SSL connection
+        # https://docs.python.org/3/library/logging.handlers.html#logging.handlers.SMTPHandler
+        # There exists code to circumvent this https://github.com/dycw/ssl-smtp-handler but not widely adopted
         if app.config["MAIL_SERVER"] and not app.config["MAIL_USE_SSL"]:
             auth = None
             if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
@@ -41,7 +45,7 @@ def create_app(config_class=Config):
             mail_handler = SMTPHandler(
                 mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
                 fromaddr="tom.demont@protonmail.com",
-                toaddrs=app.config["ADMINS"],
+                toaddrs=app.config["ADMIN"],
                 subject="Treuf Failure",
                 credentials=auth,
                 secure=secure,
@@ -68,4 +72,5 @@ def create_app(config_class=Config):
     return app
 
 
-from app import models
+# imports at the bottom to avoid circular dependencies
+from app import models, email
