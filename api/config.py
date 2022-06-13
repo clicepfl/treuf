441e1ccb56cec74082ea9502fe34e03bfd1d69b5
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-13 20:25:01 +0200

    structure config

--- api/config.py
+++ api/config.py
@@ -2,20 +2,33 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+# loads environment variables from a .env files. This .env file should not be versioned
 load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config(object):
-    # for development, will be changed asap
+    """
+    ###################
+    TECHNICAL
+    ###################
+    """
+
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    """
+    ###################
+    MAIL SUPPORT
+    ###################
+    """
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = ["tom.demont@epfl.ch", "it.clic@epfl.ch"]
+    ADMIN = [os.environ.get("ADMIN")]
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
