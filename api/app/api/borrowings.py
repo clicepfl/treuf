439ca5a77c71ecb05de76ff8fc874f7826534494
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-16 23:04:18 +0200

    redfines api

--- api/app/api/borrowings.py
+++ api/app/api/borrowings.py
@@ -3,24 +3,43 @@ from app.api import bp
 from app.api.auth import token_auth
 
 
-@bp.route("/get_borrowings_with_item/<int:id>", methods=["GET"])
+@bp.route("/borrowings/with_item/<int:id>", methods=["GET"])
 @token_auth.login_required
 def get_borrowings_with_item(id):
     return jsonify({})
 
 
-@bp.route("/get_borrowings_for_user/<int:id>", methods=["GET"])
+@bp.route("/borrowings/for_user/<int:id>", methods=["GET"])
 @token_auth.login_required
 def get_borrowings_for_user(id):
     return jsonify({})
 
 
-@bp.route("/get_borrowings/", methods=["GET"])
+@bp.route("/borrowings/<int:id>", methods=["GET"])
+@token_auth.login_required
+def get_borrowing(id):
+    return jsonify({})
+
+
+@bp.route("/borrowings/", methods=["GET"])
 @token_auth.login_required
 def get_borrowings():
     return jsonify({})
 
-@bp.route("/borrow_item/<int:id>", methods=["GET"])
+
+@bp.route("/borrowings/<int:id>", methods=["PUT"])
 @token_auth.login_required
-def borrow_item(id):
+def modify_borrowing(id):
+    return jsonify({})
+
+
+@bp.route("/borrowings/borrow/<int:item_id>/<int:user_id>", methods=["POST"])
+@token_auth.login_required
+def borrow_item(item_id, user_id):
+    return jsonify({})
+
+
+@bp.route("/borrowings/<int:id>", methods=["DELETE"])
+@token_auth.login_required
+def delete_borrowing(id):
     return jsonify({})
