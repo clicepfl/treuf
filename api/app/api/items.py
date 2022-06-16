1f04c30e1fd307bb11bc10a7c7ae338ee320d3f6
Robb-Fr <it-8b57df418575d0508@protonmail.com>
2022-06-16 23:04:24 +0200

    redefines api

--- api/app/api/items.py
+++ api/app/api/items.py
@@ -1,21 +1,46 @@
 from flask import jsonify
 from app.api import bp
 from app.api.auth import token_auth
+from app.models import Role, Item
 
 
-@bp.route("/get_item/<int:id>", methods=["GET"])
+@bp.route("/items/<int:id>", methods=["GET"])
 @token_auth.login_required
 def get_item(id):
     return jsonify({})
 
 
-@bp.route("/get_item_image/<int:id>", methods=["GET"])
+@bp.route("/items/", methods=["GET"])
+@token_auth.login_required
+def get_items():
+    return jsonify({})
+
+
+@bp.route("items/image/<int:id>", methods=["GET"])
 @token_auth.login_required
 def get_item_image(id):
     return jsonify({})
 
 
-@bp.route("/update_item/<int:id>", methods=["PUT"])
+@bp.route("/items/", methods=["POST"])
+@token_auth.login_required(role=[Role.REUF, Role.REUF_ADMIN])
+def add_item():
+    return jsonify({})
+
+
+@bp.route("items/image/<int:id>", methods=["PUT"])
+@token_auth.login_required
+def update_item_image(id):
+    return jsonify({})
+
+
+@bp.route("/items/<int:id>", methods=["PUT"])
 @token_auth.login_required
 def update_item(id):
+    return jsonify({})
+
+
+@bp.route("/items/<int:id>", methods=["DELETE"])
+@token_auth.login_required(role=[Role.REUF, Role.REUF_ADMIN])
+def delete_item():
     return jsonify({})
